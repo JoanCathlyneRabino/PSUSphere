b7c26f080f65d2eb52d11f4e0d649447a3ee418f
joan rabino <202280093@psu.palawan.edu.ph>
2025-04-11 21:43:08 +0800

    Commiting Changes in Views

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from studentorg.models import Organization
+from studentorg.forms import Organizationform
+from django.urls import reverse_lazy
+from tying import Any
+from django.db.models.query import QuerySet
+from django.db.models import Q
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = 'home.html'
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'org_list.html'
+    paginate_by = 5
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(OrganizationList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") !=None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(name__icontains=query) |
+                            Q(description__icontains=query))
+
+class OrganizationCreateView(CreateView):
+    model = Organization
+    form_class = Organizationform
+    template_name = 'org_add.html'
+    success_url = reverse_lazy('organization-list')
+
+class OrganizationUpdateView(UpdateView):
+    model = Organization
+    form_class = Organizationform
+    template_name = 'org_edit.html'
+    success_url = reverse_lazy('organization-list')
+
+class OrganizationDeleteView(DeleteView):
+    model = Organization
+    template_name = 'org_del.html'
+    success_url = reverse_lazy('organization-list')
