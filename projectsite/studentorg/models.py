ebe3efe0b95e5c52c218134fec9bac0a1ee6dd79
joan rabino <202280093@psu.palawan.edu.ph>
2025-05-18 18:42:20 +0800

    commiting changes

--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -1,4 +1,4 @@
-from django.db import models # type: ignore
+from django.db import models
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
@@ -6,27 +6,29 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
 class College(BaseModel):
     college_name = models.CharField(max_length=150)
 
     def __str__(self):
         return self.college_name
-
+    
 class Program(BaseModel):
     prog_name = models.CharField(max_length=150)
     college = models.ForeignKey(College, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.prog_name
-
+    
 class Organization(BaseModel):
     name = models.CharField(max_length=250)
-    college = models.ForeignKey(College, null=True, blank=True, on_delete=models.CASCADE)
+    college = models.ForeignKey(
+        College, null=True, blank=True, on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
-
+    
 class Student(BaseModel):
     student_id = models.CharField(max_length=15)
     lastname = models.CharField(max_length=25)
@@ -36,8 +38,9 @@ class Student(BaseModel):
 
     def __str__(self):
         return f"{self.lastname}, {self.firstname}"
-
+    
 class OrgMember(BaseModel):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     date_joined = models.DateField()
+    
